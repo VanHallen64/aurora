d4e172dab45583157737b79133df20042127603b
Allen <allen64avd@gmail.com>
2021-08-09 23:38:31 -0700

    setup log in form

--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -1,40 +1,99 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import client from "../feathers";
 
 class LoginButton extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: "",
+			name: "",
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.register = this.register.bind(this);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.register();
+	}
+
+	register() {
+		const credentials = this.state;
+		// Try to authenticate using an existing token
+		client.reAuthenticate();
+
+		try {
+			// Otherwise log in with the `local` strategy using the credentials we got
+			client.authenticate({
+				strategy: "local",
+				...credentials,
+			});
+			// If successful, show the chat page
+			console.log("User Logged In");
+		} catch (error) {
+			// If we got an error, show the login page
+			console.log(error);
+		}
+	}
+
 	render() {
 		return (
-			<main className="login container">
-				<div className="row">
-					<div className="col-12 col-6-tablet push-3-tablet text-center heading">
-						<h1 className="font-100">Log in or signup</h1>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-12 col-6-tablet push-3-tablet col-4-desktop push-4-desktop">
-						<form className="form">
-							<fieldset>
-								<input className="block" type="email" name="email" placeholder="email" />
-							</fieldset>
-
-							<fieldset>
-								<input className="block" type="password" name="password" placeholder="password" />
-							</fieldset>
-
-							<button type="button" id="login" className="button button-primary block signup">
-								Log in
+			<div className="login container">
+				<fieldset className="border p-3 rounded">
+					<legend className={`border rounded p-1 text-center`}></legend>
+					<form onSubmit={this.handleSubmit} noValidate autoComplete="off">
+						<div className="form-group">
+							<label htmlFor="inputForEmail">Email address</label>
+							<span className="mandatory">*</span>
+							<input
+								id="inputForEmail"
+								name="email"
+								type="email"
+								className="form-control"
+								aria-describedby="Enter email address"
+								placeholder="Enter email address"
+								value={this.state.email}
+								onChange={(e) => this.setState({ email: e.target.value })}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="inputForName">Your Name</label>
+							<span className="mandatory">*</span>
+							<input
+								id="inputForName"
+								type="text"
+								className="form-control"
+								aria-describedby="Enter your name"
+								placeholder="Enter your name"
+								value={this.state.name}
+								onChange={(e) => this.setState({ name: e.target.value })}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="inputForPassword">Password</label>
+							<span className="mandatory">*</span>
+							<input
+								type="password"
+								className="form-control"
+								id="inputForPassword"
+								placeholder="Enter password"
+								value={this.state.password}
+								onChange={(e) => this.setState({ password: e.target.value })}
+							/>
+						</div>
+						<div className="d-flex align-items-center justify-content-center">
+							<button type="submit" className="btn btn-outline-primary">
+								Submit
 							</button>
-
-							<button type="button" id="signup" className="button button-primary block signup">
-								Sign up and log in
+							<button className="btn btn-link">
+								<Link to="/">Cancel</Link>
 							</button>
-
-							<a className="button button-primary block" href="/oauth/github">
-								Login with GitHub
-							</a>
-						</form>
-					</div>
-				</div>
-			</main>
+						</div>
+					</form>
+				</fieldset>
+			</div>
 		);
 	}
 }
--- frontend/src/components/NavBarLogin.js
+++ frontend/src/components/NavBarLogin.js
@@ -4,18 +4,18 @@ import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
 
 function NavBarLogin() {
-	const [loginState, setLogin] = useState(undefined);
+	const [loginState, setLogin] = useState(null);
 
-	useEffect(() => {
-		// Try to authenticate with the JWT stored in localStorage
-		client.authenticate().catch(() => setLogin(null));
+	// useEffect(() => {
+	// 	// Try to authenticate with the JWT stored in localStorage
+	// 	client.authenticate().catch(() => setLogin(null));
 
-		// On successfull login ...
-		client.on('authenticated', login => {
-			// ... update the state
-			setLogin(login);
-		});
-	});
+	// 	// On successfull login ...
+	// 	client.on('authenticated', login => {
+	// 		// ... update the state
+	// 		setLogin(login);
+	// 	});
+	// });
 	if (loginState === undefined) {
 		return <h1>Loading...</h1>
 	} else if (loginState) { // If user is logged in
